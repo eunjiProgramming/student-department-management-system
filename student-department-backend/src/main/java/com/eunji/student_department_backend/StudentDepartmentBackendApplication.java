01a9fb8ca796bc37ecf9d0046f68f11e46dc1bb2
eunjiProgramming <estelle.programming@gmail.com>
2024-07-13 21:42:12 +0900

    데이터베이스 연결

---
+++ student-department-backend/src/main/java/com/eunji/student_department_backend/StudentDepartmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eunji.student_department_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDepartmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDepartmentBackendApplication.class, args);
+	}
+
+}
---
+++ student-department-backend/src/test/java/com/eunji/student_department_backend/StudentDepartmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunji.student_department_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDepartmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
