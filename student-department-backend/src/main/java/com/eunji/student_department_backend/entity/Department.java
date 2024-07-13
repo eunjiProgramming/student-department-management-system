4e6eb58c61b08d54c44f9b079db7d284dbca65ef
eunjiProgramming <estelle.programming@gmail.com>
2024-07-13 22:06:23 +0900

    entity 클래스 생성

---
+++ student-department-backend/src/main/java/com/eunji/student_department_backend/entity/Department.java
@@ -0,0 +1,32 @@
+package com.eunji.student_department_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "department_name")
+    private String departmentName;
+
+    @Column(name = "department_description")
+    private String departmentDescription;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Student> students = new ArrayList<>();
+}
---
+++ student-department-backend/src/main/java/com/eunji/student_department_backend/entity/Student.java
@@ -0,0 +1,39 @@
+package com.eunji.student_department_backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotBlank
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Email
+    @NotBlank
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+}
