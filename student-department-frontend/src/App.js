aa0148486c9d45a2891439d76474cbe10df9e8a4
eunjiProgramming <estelle.programming@gmail.com>
2024-07-14 01:25:38 +0900

    Header

--- student-department-frontend/src/App.js
+++ student-department-frontend/src/App.js
@@ -1,8 +1,13 @@
+import Header from './components/Header/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-      <h1>hi</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ student-department-frontend/src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Header.css';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <div className='container'>
+        <h1 className='title'>한국대학교</h1>
+        <nav className='management-nav'>
+          <ul className='management-ul'>
+            <li className='student-management'>
+              <NavLink
+                to='/students'
+                className='link'
+                activeClassName='active'
+                exact
+              >
+                학생관리
+              </NavLink>
+            </li>
+            <li className='department-management'>
+              <NavLink
+                to='/departments'
+                className='link'
+                activeClassName='active'
+                exact
+              >
+                학과관리
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
